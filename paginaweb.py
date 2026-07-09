e79e2a95f62e5c89cb22ebac2e47cb43a06bfa6c
ghostarm23 <bkm279684s@privaterelay.appleid.com>
2026-07-09 00:45:37 -0600

    Update paginaweb.py

--- paginaweb.py
+++ paginaweb.py
@@ -81,6 +81,22 @@ CATALOGO = {
                 "https://belcorpmexico.vtexassets.com/arquivos/ids/1157845-1600-auto?v=639175215756930000&width=1600&height=auto&aspect=true",
                 "https://belcorpmexico.vtexassets.com/arquivos/ids/1157846-1600-auto?v=639175215757900000&width=1600&height=auto&aspect=true"
             ]
+        },
+        {
+            "id": "perf_1",
+            "tag": "Top Sellers",
+            "title": "Bleu Night Perfume para Hombre 100 ml.",
+            "subtitle": "Perfume de alta concentración",
+            "price": "$663.00 MXN",
+            "precio_anterior": "$780.00 MXN",
+            "calificacion": "⭐⭐⭐⭐⭐",
+            "descripcion": "Perfume fresco para hombre de muy alta concentración de la familia olfativa herbal maderoso. Notas intensas que perduran todo el día.",
+            "imagen": "https://belcorpmexico.vtexassets.com/arquivos/ids/1157847-500-auto?v=639175215758830000&width=500&height=auto&aspect=true", # Reemplazar por tu imagen ej: "bleunight.jpg"
+            "galeria": [
+                "https://belcorpmexico.vtexassets.com/arquivos/ids/1157844-1600-auto?v=639175215755400000&width=1600&height=auto&aspect=true",
+                "https://belcorpmexico.vtexassets.com/arquivos/ids/1157845-1600-auto?v=639175215756930000&width=1600&height=auto&aspect=true",
+                "https://belcorpmexico.vtexassets.com/arquivos/ids/1157846-1600-auto?v=639175215757900000&width=1600&height=auto&aspect=true"
+            ]
         }
     ]
 }
